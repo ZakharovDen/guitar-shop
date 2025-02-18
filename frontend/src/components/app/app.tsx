4e1f9a8a9f848d72305bfe699ff6a15cf73af695
Zakharov Denis <zdv.work@yandex.ru>
2025-02-18 15:23:40 +0600

    нарезка страниц

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoute } from '../../constant';
 import LoginScreen from '../../pages/login-screen/login-screen';
 import ErrorScreen from '../../pages/error-screen/error-screen';
+import RegistrationScreen from '../../pages/registration-screen/registration-screen';
+import ProductListScreen from '../../pages/product-list-screen/product-list-screen';
+import ProductScreen from '../../pages/product-screen/product-screen';
+import Layout from '../layout/layout';
 
 function App(): JSX.Element {
   return (
@@ -9,12 +13,29 @@ function App(): JSX.Element {
       <Routes>
         <Route
           path={AppRoute.Main}
-          element={<LoginScreen />}
-        />
-        <Route
-          path="*"
-          element={<ErrorScreen />}
-        />
+          element={<Layout />}
+        >
+          <Route
+            index
+            element={<LoginScreen />}
+          />
+          <Route
+            path={AppRoute.Registration}
+            element={<RegistrationScreen />}
+          />
+          <Route
+            path={AppRoute.ProductList}
+            element={<ProductListScreen />}
+          />
+          <Route
+            path={AppRoute.ProductInfo}
+            element={<ProductScreen />}
+          />
+          <Route
+            path="*"
+            element={<ErrorScreen />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/product-screen/product-screen.tsx
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom";
+import { AppRoute } from "../../constant";
+
+function ProductScreen(): JSX.Element {
+  return (
+    <main className="page-content">
+      <div className="container">
+        <h1 className="page-content__title title title--bigger">Товар</h1>
+        <ul className="breadcrumbs page-content__breadcrumbs">
+          <li className="breadcrumbs__item" key={1}>
+            <Link className="link" to={AppRoute.Main}>Главная</Link>
+          </li>
+          <li className="breadcrumbs__item" key={2}>
+            <Link className="link" to={AppRoute.ProductList}>Каталог</Link>
+          </li>
+          <li className="breadcrumbs__item" key={3}>
+            <Link className="link" to={AppRoute.ProductInfo}>Товар</Link>
+          </li>
+        </ul>
+        <section className="product-container"><img className="product-container__img" src="img/content/catalog-product-1.png" srcSet="img/content/catalog-product-1@2x.png 2x" width="90" height="235" alt=""></img>
+          <div className="product-container__info-wrapper">
+            <h2 className="product-container__title title title--big title--uppercase">СURT Z30 Plus</h2>
+            <br></br>
+            <br></br>
+            <div className="tabs"><a className="button button--medium tabs__button" href="#characteristics">Характеристики</a><a className="button button--black-border button--medium tabs__button" href="#description">Описание</a>
+              <div className="tabs__content" id="characteristics">
+                <table className="tabs__table">
+                  <tr className="tabs__table-row">
+                    <td className="tabs__title">Артикул:</td>
+                    <td className="tabs__value">SO754565</td>
+                  </tr>
+                  <tr className="tabs__table-row">
+                    <td className="tabs__title">Тип:</td>
+                    <td className="tabs__value">Электрогитара</td>
+                  </tr>
+                  <tr className="tabs__table-row">
+                    <td className="tabs__title">Количество струн:</td>
+                    <td className="tabs__value">6 струнная</td>
+                  </tr>
+                </table>
+                <p className="tabs__product-description hidden">Гитара подходит как для старта обучения, так и для домашних занятий или использования в полевых условиях, например, в походах или для проведения уличных выступлений. Доступная стоимость, качество и надежная конструкция, а также приятный внешний вид, который сделает вас звездой вечеринки.</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default ProductScreen;
